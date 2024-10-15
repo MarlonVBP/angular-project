8394306a3ab339bcb88d4373fb6108199d1965f7
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-15 09:34:39 -0300

    feat: login component

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor() {}
 
+  eye: string = 'images/eye-open.svg'
+  typeInput: string = 'password'
+  showPassword(){
+    this.typeInput = this.typeInput == 'password' ? 'text' : 'password';
+    this.eye = this.eye == 'images/eye-open.svg' ? 'images/eye-close.svg' : 'images/eye-open.svg'
+  }
 }
