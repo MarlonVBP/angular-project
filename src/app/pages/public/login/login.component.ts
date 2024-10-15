cb13b0503105025810df11a34f07b79bca1c8196
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-15 17:44:20 -0300

    #

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -9,11 +9,4 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   constructor() {}
-
-  eye: string = 'images/eye-open.svg'
-  typeInput: string = 'password'
-  showPassword(){
-    this.typeInput = this.typeInput == 'password' ? 'text' : 'password';
-    this.eye = this.eye == 'images/eye-open.svg' ? 'images/eye-close.svg' : 'images/eye-open.svg'
-  }
 }
