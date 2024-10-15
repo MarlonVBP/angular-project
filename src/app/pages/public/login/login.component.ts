1e855d23c12e7de3d9ab5e969782121377cf2106
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-15 12:32:05 -0300

    :sparkles: feat: finished structure and styles

--- src/app/pages/public/login/login.component.ts
+++ src/app/pages/public/login/login.component.ts
@@ -1,19 +1,47 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor() {}
+  constructor(private router: Router) {}
 
-  eye: string = 'images/eye-open.svg'
-  typeInput: string = 'password'
-  showPassword(){
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(255),
+    ]),
+  });
+
+  eye: string = 'assets/images/eye-open.svg';
+  typeInput: string = 'password';
+  showPassword() {
     this.typeInput = this.typeInput == 'password' ? 'text' : 'password';
-    this.eye = this.eye == 'images/eye-open.svg' ? 'images/eye-close.svg' : 'images/eye-open.svg'
+    this.eye =
+      this.eye == 'assets/images/eye-open.svg'
+        ? 'assets/images/eye-close.svg'
+        : 'assets/images/eye-open.svg';
+  }
+
+  signUp() {
+    this.router.navigate(['sign-up']);
+  }
+
+  onSubmit() {
+    console.log('submit')
   }
 }
