bfd4d3cdd34b95be4e6a7d650c9b8624d43822ab
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-16 18:35:00 -0300

    :rocket feat: header component finished

--- src/app/components/public/header-page/header-page.component.ts
+++ src/app/components/public/header-page/header-page.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header-page.component.html',
-  styleUrl: './header-page.component.scss'
+  styleUrl: './header-page.component.scss',
 })
 export class HeaderPageComponent {
+  activeUrl: string = '';
 
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.activeUrl = this.router.url;
+        console.log(this.activeUrl);
+      });
+  }
+
+  isActive = false; // Estado inicial do menu
+
+  toggleMenu() {
+    this.isActive = !this.isActive; // Alterna o estado do menu
+  }
 }
