2f9724e8621990e1d52377454e41fbf78b2882a5
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-17 17:43:21 -0300

    #

--- src/app/pages/public/contato/contato.component.ts
+++ src/app/pages/public/contato/contato.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../components/public/footer/footer.component";
+import { HeaderPageComponent } from "../../../components/public/header-page/header-page.component";
 
 @Component({
   selector: 'app-contato',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderPageComponent],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
--- src/app/pages/public/sobre-nos/sobre-nos.component.ts
+++ src/app/pages/public/sobre-nos/sobre-nos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../components/public/footer/footer.component";
+import { HeaderPageComponent } from "../../../components/public/header-page/header-page.component";
 
 @Component({
   selector: 'app-sobre-nos',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderPageComponent],
   templateUrl: './sobre-nos.component.html',
   styleUrl: './sobre-nos.component.scss'
 })
