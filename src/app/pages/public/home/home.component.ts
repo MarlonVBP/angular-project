c69ddf22027f79de6e1259597e6d49706f0f2f37
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-17 08:51:05 -0300

    :rocket: feat: organizing the home page layout

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
 import { FooterComponent } from "../../../components/public/footer/footer.component";
 import { HeaderPageComponent } from "../../../components/public/header-page/header-page.component";
+import { CardsReceitasComponent } from "../../../components/public/cards-receitas/cards-receitas.component";
+import { CommonModule } from '@angular/common';
+import { Receita } from '../../../models/receita';
+import { ReceitasService } from '../../../services/receitas.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, HeaderPageComponent],
+  imports: [FooterComponent, HeaderPageComponent, CardsReceitasComponent, CommonModule],
   animations: [
     trigger('fadeIn', [
       transition(':enter', [
@@ -21,4 +25,17 @@ import { HeaderPageComponent } from "../../../components/public/header-page/head
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private receitaService: ReceitasService){
+    this.load();
+  }
+
+  load(){
+    this.receitaService.select().subscribe((response: any)=>{
+      // this.receitas = response;
+      console.log(response)
+    })
+  }
+
+  receitas: Receita[] = [];
+}
