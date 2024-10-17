3d431717b572a4056d2850e47b7048ed300ea353
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-17 11:18:07 -0300

    :rocket: feat: styling the card

--- src/app/components/public/cards-receitas/cards-receitas.component.ts
+++ src/app/components/public/cards-receitas/cards-receitas.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import { Receita } from '../../../models/receita';
+import { TextEllipsisPipe } from '../../../pipes/text-ellipsis.pipe';
 
 @Component({
   selector: 'app-cards-receitas',
   standalone: true,
-  imports: [],
+  imports: [TextEllipsisPipe],
   templateUrl: './cards-receitas.component.html',
   styleUrl: './cards-receitas.component.scss',
 })
