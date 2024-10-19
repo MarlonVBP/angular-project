7d94d034c9685c37c18ecf66449c4a9d68515c48
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-19 18:57:45 -0300

    :rocket: feat: router guard finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { SignUpComponent } from './pages/public/sign-up/sign-up.component';
 import { PerfilComponent } from './pages/public/perfil/perfil.component';
 import { ContatoComponent } from './pages/public/contato/contato.component';
 import { SobreNosComponent } from './pages/public/sobre-nos/sobre-nos.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page',
+    canActivate: [authGuard],
   },
   {
     path: 'login',
@@ -26,20 +28,24 @@ export const routes: Routes = [
     path: 'perfil',
     component: PerfilComponent,
     title: 'Perfil page',
+    canActivate: [authGuard],
   },
   {
     path: 'contato',
     component: ContatoComponent,
     title: 'Contato page',
+    canActivate: [authGuard],
   },
   {
     path: 'sobre-nos',
     component: SobreNosComponent,
     title: 'Perfil page',
+    canActivate: [authGuard],
   },
   {
     path: '**',
     component: HomeComponent,
     title: 'Home page',
+    canActivate: [authGuard],
   },
 ];
